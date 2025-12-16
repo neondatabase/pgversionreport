e12e2e09a64b0b6e36b8c240b8ac1e62d3ac9f80
Astrid Gealer <astrid@gealer.email>
2025-12-16 18:00:49 +0000

    update data

---
+++ scripts/splitToMultipleFiles.ts
@@ -0,0 +1,37 @@
+import { join } from "path";
+import releaseNotesFormatted from "../scraper/release_notes_formatted.json" assert { type: "json" };
+import { writeFile, writeFileSync } from "fs";
+
+console.log("Splitting release notes into multiple files...");
+
+const root = join(
+    __dirname,
+    "..",
+    "src",
+    "data",
+);
+
+writeFileSync(
+    join(root, "pg_release_data.json"),
+    JSON.stringify(releaseNotesFormatted, null, 4),
+);
+
+const versionDates = { ...releaseNotesFormatted.versionDates };
+for (const [version, date] of Object.entries(versionDates)) {
+    try {
+        // YYYY-MM-DD format
+        const d = new Date(date as string);
+        if (isNaN(d.getTime())) {
+            throw new Error("Invalid date");
+        }
+    } catch {
+        console.warn(`Invalid date for version ${version}: ${date}`);
+        // @ts-expect-error
+        delete versionDates[version];
+    }
+}
+
+writeFileSync(
+    join(root, "version_dates.json"),
+    JSON.stringify(versionDates, null, 4),
+);
